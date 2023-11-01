0d4ed4e93383124530e1243ec32b18dee1a3d9b3
songyuheon98 <songyuheon2750@gmail.com>
2023-11-01 09:06:53 +0900

    Add 삼성싸피 알고리즘 1 ~ 5

---
+++ src/Sol1.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+
+class Sol1
+{
+    public static void main(String args[]) throws Exception
+    {
+        Scanner sc = new Scanner(System.in);
+        int T;
+        T=sc.nextInt();
+        for(int test_case = T; test_case >= 0; test_case--)
+            System.out.print(test_case+" ");
+    }
+}
